25d86769d328cd21a4962eb6d1a36b20e1f73356
Randy1510 <109732904+Randy1510@users.noreply.github.com>
2024-10-23 23:21:39 +0530

    Day1 homework

---
+++ homework.js
@@ -0,0 +1,18 @@
+var firstName = "Rathinavel T K"
+var companyName = "KPMG Global Services"
+var mobileNumber = 8122374458
+var isAutomation = true
+var hasPlaywright
+
+
+console.log(firstName)
+console.log(companyName)
+console.log(mobileNumber)
+console.log(isAutomation)
+console.log(hasPlaywright)
+
+console.log(typeof firstName)
+console.log(typeof companyName)
+console.log(typeof mobileNumber)
+console.log(typeof isAutomation)
+console.log(typeof hasPlaywright)
